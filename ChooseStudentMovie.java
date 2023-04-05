c03bbfbcb5d431e7d6315619ff5c8142a1a8b760
Seth-Tedder <126044310+Seth-Tedder@users.noreply.github.com>
2023-04-05 10:50:04 -0400

    Add files via upload

---
+++ ChooseStudentMovie.java
@@ -0,0 +1,33 @@
+package lab9;
+
+import java.util.*;
+
+public class ChooseStudentMovie {
+	TreeMap<String, String[]> studentMovies = new TreeMap<String, String[]>();
+	
+	public class RankedMov {
+		String name;
+		int rank;
+		
+		public RankedMov(String name) {
+			this.name = name;
+			rank = 0;
+		}
+	}
+	
+	public ChooseStudentMovie() {
+		
+	}
+	
+	public void read(String stuMov) {
+		
+	}
+	
+	public String chooseMovie(String[] students) {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
