88cd879b88379bed376e3953339ece6256fb886c
Seth-Tedder <126044310+Seth-Tedder@users.noreply.github.com>
2023-04-05 12:43:09 -0400

    Update ChooseStudentMovie.java

--- ChooseStudentMovie.java
+++ ChooseStudentMovie.java
@@ -5,13 +5,22 @@ import java.util.*;
 public class ChooseStudentMovie {
 	TreeMap<String, String[]> studentMovies = new TreeMap<String, String[]>();
 	
-	public class RankedMov {
-		String name;
-		int rank;
+	public class RankedMov implements Comparable<RankedMov> {
+		public String name;
+		public int rank;
 		
 		public RankedMov(String name) {
 			this.name = name;
 			rank = 0;
+		}
+		public void tick() {
+			rank++;
+		}
+		public void reset() {
+			rank = 0;
+		}
+		public int compareTo(RankedMov mov) {
+			return this.rank - mov.rank;
 		}
 	}
 	
